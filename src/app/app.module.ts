a6c0c872a3f6cdba6ab9fc6aa6a7e6b7601f6de2
wiktortr <wiktortr9@gmail.com>
2021-08-03 21:04:22 +0200

    Create globe compoent and install three.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GlobeComponent } from './components/globe/globe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    GlobeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/globe/globe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three'
+
+@Component({
+  selector: 'app-globe',
+  templateUrl: './globe.component.html',
+  styleUrls: ['./globe.component.scss']
+})
+export class GlobeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
