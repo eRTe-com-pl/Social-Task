eb93be31ebeccd97dd45321c6bd6091e8d8fe4ca
wiktortr <wiktortr9@gmail.com>
2021-08-04 11:56:31 +0200

    Setup sdk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA2xrOqX778_rGritFulTazpLf5eIVDJNs",
+    authDomain: "social-tasks-48cc0.firebaseapp.com",
+    databaseURL: "https://social-tasks-48cc0-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "social-tasks-48cc0",
+    storageBucket: "social-tasks-48cc0.appspot.com",
+    messagingSenderId: "1057900347027",
+    appId: "1:1057900347027:web:feb7c52244aed46cb1d766",
+    measurementId: "G-NWCZJE2RQM"
+  }
 };
