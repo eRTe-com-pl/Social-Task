1bc422aaa528cdd98ba04dd1fc8f3d2e37b227b7
Rafal Taraszka <mail.rtaraszka@gmail.com>
2021-09-18 14:52:26 +0200

    Logout Show or hiding login form

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Router } from "@angular/router";
 import { AngularFirestore, AngularFirestoreDocument } from "@angular/fire/firestore";
-import  firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
-import { Observable,of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
-// import 'rxjs/add/operator/switchMap';
-
 import 'rxjs/add/operator/switchMap';
 import { switchMap } from "rxjs/operators";
+import { first } from 'rxjs-compat/operator/first';
 
 @Injectable({
   providedIn: 'root'
@@ -20,44 +19,57 @@ export class AuthService {
     private router: Router
   ) { }
 
-  login(email: string, password: string){
+  emailLogin(email: string, password: string) {
     this.ofAuth.signInWithEmailAndPassword(email, password)
-    .then(value => {
-      console.log('Nice, it worked!');
-      this.router.navigateByUrl('/profile');
-    })
-    .catch(err => {
-      console.log("Something went wrong", err.message);
-    });
+      .then(value => {
+        // this.isLoggedIn_ = true;
+        this.router.navigateByUrl('/profile');
+      })
+      .catch(err => {
+        console.log("Something went wrong", err.message);
+      });
   }
 
-  emailSignup(email:string,password: string){
-    this.ofAuth.createUserWithEmailAndPassword(email,password)
-    .then(value=> {
-      console.log('Succes', value);
-      this.router.navigateByUrl('/profile');
-    })
-    .catch(error => {
-      console.log('Something wrong', error)
-    });
+  emailSignup(email: string, password: string) {
+    this.ofAuth.createUserWithEmailAndPassword(email, password)
+      .then(value => {
+        // this.isLoggedIn_ = true;
+        this.router.navigateByUrl('/profile');
+      })
+      .catch(error => {
+        console.log('Something wrong', error)
+      });
   }
 
-  googleLogin(){
+  googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider();
     return this.oAuthLogin(provider)
-    .then(value => {
-      console.log("Succes", value),
-      this.router.navigateByUrl('/home');
-    })
+      .then(value => {
+        this.router.navigateByUrl('/login');
+      })
   }
 
-  logout(){
+  private oAuthLogin(provider: firebase.auth.AuthProvider) {
+    return this.ofAuth.signInWithPopup(provider);
+  }
+
+  logout() {
     this.ofAuth.signOut().then(() => {
-      this.router.navigate(['/']);
+      console.log('Logout..');
+      console.log(this.isLoggedIn);
+      this.router.navigate(['/login']);
     });
   }
 
-  private oAuthLogin(provider: firebase.auth.AuthProvider){
-    return this.ofAuth.signInWithPopup(provider);
+
+  isLoggedIn() {
+    // return this.ofAuth.onAuthStateChanged
+    // var user = firebase.auth().currentUser;
+    // return this.isLoggedIn_;
+    // if (user) {
+    //User is signed in.
+    // } else {
+    //No user is signed in.
+    // }
   }
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../auth.service";
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login-page',
@@ -7,13 +8,16 @@ import { AuthService } from "../auth.service";
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor(private authService: AuthService) { }
-
+  constructor(private authService: AuthService, public afAuth: AngularFireAuth) { }
+  
   ngOnInit(): void {
   }
-
+  
   loginGoogle(){
     this.authService.googleLogin();
+  }
+  
+  signOut() {
+    this.authService.logout();
   }
 }
