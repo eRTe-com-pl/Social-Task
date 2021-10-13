f3dca909115b8769bef81e8607794bb9badce2f3
Rafal Taraszka <mail.rtaraszka@gmail.com>
2021-10-13 17:36:11 +0200

    Instal socket and express Connetct to socket on port 3000 "npm server.js"

---
+++ server.js
@@ -0,0 +1,30 @@
+const express   = require("express");
+const path      = require('path');
+const http      = require('http');
+const socketIO = require('socket.io');
+// const { Socket } = require("dgram");
+
+const app       = express();
+const port      = process.env.PORT || 3000 ;
+
+
+app.use(express.static(path.join(__dirname, 'dist/Social-Task')));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'dist/Social-Task/index.html'));
+});
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect', ()=>{
+        console.log('User dissconnected');
+    });
+});
+
+server.listen(port, ()=> {
+    console.log(`Server running :) on port ${port}`);
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {io} from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent { }
+export class AppComponent { 
+  socket;
+  // numberOfOnlineUsers: number;
+
+  constructor() {
+    this.socket = io();
+  }
+
+}
